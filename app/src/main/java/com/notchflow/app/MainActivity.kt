a6440b89698a3c9902ea7821e429c5af04e9f7f9
pabelpi <piedrapinap@gmail.com>
2026-06-11 07:31:17 -0400

    Create MainActivity.kt

---
+++ app/src/main/java/com/notchflow/app/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.notchflow.app
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.startButton).setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+                startActivity(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName")))
+            } else {
+                startService(Intent(this, Pixel10DynamicIslandService::class.java))
+            }
+        }
+    }
+}
+
